0358fb7b9a55efd5962fc2b3f0557345e2baa9db
bluewt129 <bluewt129@gmail.com>
2024-10-28 00:09:00 +0900

    feat: C# 01

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace CSharp
+﻿using System.ComponentModel;
+
+namespace CSharp
 {
     class Program
     {
         static void Main(string[] args)
          {
-            Console.WriteLine("Hello, World");
+            int hp = 100;
+            int maxHp = 100;
+
+            string message = string.Format("당신의 HP는 {0} {1} 입니다", hp, maxHp);
+
+            string mms = $"당신의 체력은 {hp}/{maxHp} 입니다";
+            Console.WriteLine(message);
+            Console.WriteLine(mms);
+
+            bool isAlive = (hp > 0);
+
+            int level = 10;
+
+            bool isTall = true;
+            bool isSmart = false;
+
+            bool result = (isTall || isSmart);
+            Console.WriteLine(result);
+
+            bool result1 = (isTall && isSmart);
+            Console.WriteLine(result1);
+            
         }
     }
 }
